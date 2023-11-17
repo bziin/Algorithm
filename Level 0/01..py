18a916399ba73514d3f82821a3eaaeb0fd782b5a
bzzzzi@naver.com <bzzzzi@naver.com>
2023-11-17 18:26:45 +0900

    Create 01..py

---
+++ Level 0/01..py
@@ -0,0 +1,20 @@
+
+
+n=int(input())
+a=list(map(int,input().split()))
+
+ave=round(sum(a)/n)
+min=21470000000
+
+for idx,x in enumerate(a):
+    tmp=abs(x-ave)
+    if tmp<min:
+        min=tmp
+        score=x
+        res=idx+1
+    elif tmp == min:
+        if x>score:
+            score= x
+            res=idx+1
+
+print(ave,res)
